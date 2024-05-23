2e93ad101cc5300b6cf3efe3d6947c1e529a3975
eakudompong.chanoknan <ceguantz96@outlook.com>
2024-05-23 12:09:36 +0900

    add routing might change later

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,30 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+export default function TabLayout() {
+
+    return (
+        <Tabs
+        >
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="data"
+                options={{
+                    title: 'Analytics',
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
+}
