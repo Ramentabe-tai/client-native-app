a13c0c52a33770f1bb71050d1e75bdf6028d9bb2
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-06-10 22:35:54 +0900

    add victoryCharts

--- components/charts/LineChart.tsx
+++ components/charts/LineChart.tsx
@@ -1,11 +1,34 @@
-import { View, Text } from "react-native";
+import { center } from "@shopify/react-native-skia";
 import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 }
+];
 
 export default function LineChart() {
   return (<>
     <Text>LineChart:</Text>
-    <View style={{ backgroundColor: '#7777', height: 300, marginHorizontal: 10, borderRadius: 10 }}>
+    <View style={styles.container}>
+      <VictoryChart width={380} theme={VictoryTheme.material}>
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
     </View>
   </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    height: 320,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
