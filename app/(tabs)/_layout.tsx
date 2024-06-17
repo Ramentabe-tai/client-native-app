345e4abc47e1e70d7e008a40f68852b184c997a1
eakudompong.chanoknan <ceguantz96@outlook.com>
2024-06-17 15:00:36 +0900

    fix layouts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,7 +5,15 @@ import {
 } from "@react-navigation/material-top-tabs";
 import { ParamListBase, TabNavigationState } from "@react-navigation/native";
 import { withLayoutContext } from "expo-router";
-import React from "react";
+import React, { useRef, useCallback, useState } from "react";
+import FloatButton from "@/components/fab/FloatButton";
+import BottomSheet, { BottomSheetBackdrop, } from "@gorhom/bottom-sheet";
+import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
+import { BottomSheetDefaultBackdropProps } from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types";
+import { Portal, Provider } from 'react-native-paper';
+import Saving from "@/components/bottomSheets/Saving";
+import Expanse from "@/components/bottomSheets/Expanse";
+import { View, StyleSheet } from "react-native";
 
 const { Navigator } = createMaterialTopTabNavigator();
 
@@ -17,14 +25,88 @@ export const MaterialTopTabs = withLayoutContext<
 >(Navigator);
 
 const TabLayout = () => {
+
+  const [isOpened, setIsOpened] = useState(false);
+
+  const savingSheetRef = useRef<BottomSheetMethods>(null);
+  const expanseSheetRef = useRef<BottomSheetMethods>(null);
+
+  const handleOpenSavingSheet = useCallback(() => {
+    savingSheetRef.current?.expand();
+  }, []);
+
+  const handleOpenExpanseSheet = useCallback(() => {
+    expanseSheetRef.current?.expand();
+  }, []);
+
+  const renderBackdrop = useCallback(
+    (
+      props: React.JSX.IntrinsicAttributes & BottomSheetDefaultBackdropProps
+    ) => (
+      <BottomSheetBackdrop
+        {...props}
+        disappearsOnIndex={-1}
+        appearsOnIndex={1}
+        opacity={0.5}
+        onPress={() => {
+          savingSheetRef.current?.close();
+          expanseSheetRef.current?.close();
+        }}
+      />
+    ),
+    []
+  );
+
   return (
-    <MaterialTopTabs
-      screenOptions={{
-        tabBarActiveTintColor: "#131620",
-        tabBarPressColor: "transparent",
-      }}
-    ></MaterialTopTabs>
+    <Provider>
+      <MaterialTopTabs
+        screenOptions={{
+          tabBarActiveTintColor: "#131620",
+          tabBarPressColor: "transparent",
+        }}
+      ></MaterialTopTabs>
+
+      <FloatButton
+        onOpenSavingAction={handleOpenSavingSheet}
+        onOpenExpanseAction={handleOpenExpanseSheet}
+        setIsOpened={setIsOpened}
+        isOpened={isOpened}
+      />
+      <Portal>
+        <BottomSheet
+          ref={savingSheetRef}
+          snapPoints={['50%']}
+          index={-1}
+          backdropComponent={renderBackdrop}
+          enablePanDownToClose
+          style={styles.bottomSheet}
+        >
+          <View >
+            <Saving />
+          </View>
+        </BottomSheet>
+        <BottomSheet
+          ref={expanseSheetRef}
+          snapPoints={['60%']}
+          index={-1}
+          backdropComponent={renderBackdrop}
+          enablePanDownToClose
+          style={styles.bottomSheet}
+          keyboardBehavior="extend"
+        >
+          <View >
+            <Expanse />
+          </View>
+        </BottomSheet>
+      </Portal>
+    </Provider>
   );
 };
+
+const styles = StyleSheet.create({
+  bottomSheet: {
+    zIndex: 10,
+  },
+});
 
 export default TabLayout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,123 +1,44 @@
-import React, { useRef, useCallback, useState } from "react";
+import React from "react";
 import { Stack } from "expo-router";
-import { Image, View, StyleSheet, Text } from "react-native";
+import { Image } from "react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import FloatButton from "@/components/fab/FloatButton";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import BottomSheet, {
-  BottomSheetBackdrop,
-  BottomSheetModal,
-} from "@gorhom/bottom-sheet";
 
-import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
-import { BottomSheetDefaultBackdropProps } from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types";
-import { Portal, Provider } from 'react-native-paper';
-import Saving from "@/components/bottomSheets/Saving";
-import Expanse from "@/components/bottomSheets/Expanse";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  const [isOpened, setIsOpened] = useState(false);
-
-  const savingSheetRef = useRef<BottomSheetMethods>(null);
-  const expanseSheetRef = useRef<BottomSheetMethods>(null);
-
-  const handleOpenSavingSheet = useCallback(() => {
-    savingSheetRef.current?.expand();
-  }, []);
-
-  const handleOpenExpanseSheet = useCallback(() => {
-    expanseSheetRef.current?.expand();
-  }, []);
-
-  const renderBackdrop = useCallback(
-    (
-      props: React.JSX.IntrinsicAttributes & BottomSheetDefaultBackdropProps
-    ) => (
-      <BottomSheetBackdrop
-        {...props}
-        disappearsOnIndex={-1}
-        appearsOnIndex={1}
-        opacity={0.5}
-        onPress={() => {
-          savingSheetRef.current?.close();
-          expanseSheetRef.current?.close();
-        }}
-      />
-    ),
-    []
-  );
 
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{ flex: 1 }}>
 
-        <Provider>
-          <Stack screenOptions={{ headerShadowVisible: false }}>
-            <Stack.Screen
-              name='(tabs)'
-              options={{
-                headerLeft: () => (
-                  <Image
-                    source={{
-                      uri: "https://images.pexels.com/photos/23483902/pexels-photo-23483902/free-photo-of-a-woman-in-a-white-shirt-and-brown-boots-standing-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                    }}
-                    style={{ width: 30, height: 30, borderRadius: 20 }}
-                  />
-                ),
-                headerTitle: () => null,
-                headerTitleAlign: "center",
-              }}
-            />
-            <Stack.Screen name='error' options={{ headerTitle: "Error", headerTitleAlign: "center" }} />
-            <Stack.Screen
-              name="LoginPage"
-              options={{ headerTitle: "Login", headerTitleAlign: "center" }}
-            />
-          </Stack>
-          <FloatButton
-            onOpenSavingAction={handleOpenSavingSheet}
-            onOpenExpanseAction={handleOpenExpanseSheet}
-            setIsOpened={setIsOpened}
-            isOpened={isOpened}
+        <Stack screenOptions={{ headerShadowVisible: false }}>
+          <Stack.Screen
+            name='(tabs)'
+            options={{
+              headerLeft: () => (
+                <Image
+                  source={{
+                    uri: "https://images.pexels.com/photos/23483902/pexels-photo-23483902/free-photo-of-a-woman-in-a-white-shirt-and-brown-boots-standing-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                  }}
+                  style={{ width: 30, height: 30, borderRadius: 20 }}
+                />
+              ),
+              headerTitle: () => null,
+              headerTitleAlign: "center",
+            }}
           />
-          <Portal>
-            <BottomSheet
-              ref={savingSheetRef}
-              snapPoints={['50%']}
-              index={-1}
-              backdropComponent={renderBackdrop}
-              enablePanDownToClose
-              style={styles.bottomSheet}
-            >
-              <View >
-                <Saving />
-              </View>
-            </BottomSheet>
-            <BottomSheet
-              ref={expanseSheetRef}
-              snapPoints={['60%']}
-              index={-1}
-              backdropComponent={renderBackdrop}
-              enablePanDownToClose
-              style={styles.bottomSheet}
-              keyboardBehavior="extend"
-            >
-              <View >
-                <Expanse />
-              </View>
-            </BottomSheet>
-          </Portal>
-        </Provider>
+          <Stack.Screen name='error' options={{ headerTitle: "Error", headerTitleAlign: "center" }} />
+          <Stack.Screen
+            name="LoginPage"
+            options={{ headerTitle: "Login", headerTitleAlign: "center" }}
+          />
+        </Stack>
 
       </GestureHandlerRootView>
     </QueryClientProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  bottomSheet: {
-    zIndex: 10,
-  },
-});
+
